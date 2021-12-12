65b04113563a09c0dd7cd4e377f987cd9ab8064b
maxwell walin <maxwellwalin@gmail.com>
2021-12-12 12:43:06 -0800

    debugged animations and restructured portfolio el

--- src/components/elements/Portfolio.js
+++ src/components/elements/Portfolio.js
@@ -19,15 +19,14 @@ function Portfolio({ portfolio }) {
 
       <div className="portfolio-item rounded shadow-dark">
         <div className="details">
-          <a
-            href="#"
+          <div
             onClick={handleLightbox}
           >
             <span className="term text-capitalize">{category}</span>
             <h4 className="title">{title}</h4>
             <h5 className="title">{subtitle}</h5>
             <span className="more-button"><i className="icon-magnifier-add"></i></span>
-          </a>
+          </div>
           <span className="portfolio-links"><a href={repoLink} target="_blank" rel="noreferrer">View Repository</a> | <a href={deployedLink} target="_blank" rel="noreferrer">Deployed Website</a></span>
         </div>
         <div className="thumb">
